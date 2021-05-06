d664c2869eb9b98374364695fed28bed69fe4574
The Glass Corner <ksimons@aggies.ncat.edu>
2021-05-06 02:21:15 -0400

    added hero background video, custom styled components for landing page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import styled from 'styled-components'
 import {Button} from './Button'
+import Video from '../assets/videos/Aggieland.mp4'
+import Video2 from '../assets/videos/TherapyMovie.mp4'
 const Hero = () => {
     return (
        <HeroContainer>
            <HeroBg>
-               <VideoBg src= "" type="video/mp4"/>
+               <VideoBg src={Video} type="video/mp4" autoPlay muted loop playsInline/>
 
                
            </HeroBg>
            <HeroContent>
                <HeroItems>
-                   <HeroH1>Full Stack Software Engineer</HeroH1>
-                   <HeroP>Kellik Simons </HeroP>
-                   <Button>Get to Know Me!</Button>
+                   <HeroH1>Welcome, I'm Kellik Simons</HeroH1>
+                   <HeroP>NCA&T Computer Science Graduate & Full Stack Software Engineer </HeroP>
+                   <Button>Get To Know Me!</Button>
                </HeroItems>
            </HeroContent>
        </HeroContainer>
@@ -35,12 +37,57 @@ color: #fff;
 
 `
 
-const HeroBg = styled.div ``
+const HeroBg = styled.div `
+position: absolute;
+top: 0;
+bottom: 0;
+right: 0;
+left: 0;
+width: 100%;
+height: 100%;
+overflow: hidden;
 
-const VideoBg = styled.video``
+`
+
+const VideoBg = styled.video`
+width:100%;
+height:100%;
+-o-object-fit:cover;
+object-fit: cover;
+`
 
 
-const HeroContent = styled.div``
-const HeroItems = styled.div``
-const HeroH1 = styled.h1``
-const HeroP = styled.p``
+const HeroContent = styled.div`
+z-index:3;
+height: calc(100vh-80px);
+max-height: 100%;
+padding: 0rem calc((100vw-1300px)/2);
+
+`
+const HeroItems = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+text-align: center;
+height: 100vh;
+max-height: 100%;
+padding: 0;
+color: #fff;
+line-height: 1.1;
+font-weight: bold;
+
+
+`
+const HeroH1 = styled.h1`
+font-sized: clamp(1.5rem, 6vw, 4rem);
+margin-bottom: 1.5rem;
+letter-spacing: 3px;
+padding: 0 1rem;
+
+
+`
+const HeroP = styled.p`
+font-sized: clamp(1rem, 3vw, 3rem);
+margin-bottom: 2rem;
+`
